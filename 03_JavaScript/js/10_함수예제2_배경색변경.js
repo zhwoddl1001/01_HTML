23660f8f6cc34062b2232483e8d55ddaab700458
zhwoddl1001 <zhwoddl0723@gmail.com>
2024-11-12 17:00:25 +0900

    배열 개요 및 실습 과제 문제 풀이

---
+++ 03_JavaScript/js/10_함수예제2_배경색변경.js
@@ -0,0 +1,20 @@
+const bd = document.getElementById("bd");
+function 색상변경(abc) {
+  switch (abc) {
+    case "lightblue":
+      bd.style.backgroundColor = "lightblue";
+      break;
+    case "lightcoral":
+      bd.style.backgroundColor = "lightcoral";
+      break;
+    case "lightyellow":
+      bd.style.backgroundColor = "lightyellow";
+      break;
+    case "lightgreen":
+      bd.style.backgroundColor = "lightgreen";
+      break;
+    case "lightgrey":
+      bd.style.backgroundColor = "lightgrey";
+      break;
+  }
+}
---
+++ 03_JavaScript/js/Kht.js
@@ -0,0 +1,3 @@
+function 기능1(숫자) {
+  return Math.floor(숫자);
+}
