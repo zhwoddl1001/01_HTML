05ff30f4c1bf2f2974d597dea1cf61621885d7be
zhwoddl1001 <zhwoddl0723@gmail.com>
2024-11-15 17:40:39 +0900

    window객체 회원가입 구현

--- 실습과제/241113_약관동의.js
+++ 실습과제/241113_약관동의.js
@@ -1,6 +1,51 @@
-function check(){
-    const name = document.getElementsByName("name");
-    const hobby = document.getElementsByName("hobby");
-    const result = document.getElementById("name-div")
-    if()
+function submitForm() {
+  //필수 약관 동의 확인
+  const serviceChecked = document.getElementById("service").checked;
+  const privacyChecked = document.getElementById("privacy").checked;
+
+  //모든 필수 약관에 동의 하지 않았다면
+  // !true = false   !false = true
+  // if 안에가 true 가 되어야지 alert 동작
+  /*
+      if (serviceChecked && privacyChecked) {
+        alert("두개다 선택하셨네요.");
+      } else { // !serviceChecked && !privacyChecked
+        alert("모든 필수 약관에 동의해주세요.");
+      }
+        */
+
+  // !serviceChecked = 서비스 약관에 동의하지 않음 의미
+  // !privacyChecked = 개인정보 보호 약관에 동의하지 않음을 의미
+  // && 모든 필수 약관에 체크를 안해야지 필수 약관 동의해달라는 표기가 보임
+  // || 둘 중 하나라도 필수 약관에 체크를 하지 않았다면 모두 체크하라는
+  // 표기가 보여질 것
+  // serviceChecked 가 false 인 경우 느낌표를 붙이면 true로 변경해줌
+
+  // !serviceChecked = 서비스 항목에 체크를 안했을 때가 false인데
+  // 서비스 항목에 체크를 안했을 때가 false인데 앞에 !붙여서 true로 변한 것
+  if (!serviceChecked || !privacyChecked) {
+    alert("모든 필수 약관에 동의해주세요.");
+    return;
+  }
+
+  // 선택된 취미를 배열로 저장하는 for문 사용
+  const hchecked = document.querySelectorAll(".hobby");
+  let hobbies = "";
+  let count = 0;
+
+  for (let i = 0; i < hchecked.length; i++) {
+    if (hchecked[i].checked) {
+      hobbies += hchecked[i].value + " "; //선택한 취미를 옆으로 모두 이어 붙이기
+      count++; //취미 개수 증가
+    }
+  }
+
+  // 위 코드는 결과 표시를 보기위한 진행중
+  //결과 표시
+  const result = document.getElementById("result");
+  if (count > 0) {
+    result.innerText = `선택된 취미 : ${hobbies} \n 선택된 취미 개수 : ${count}`;
+  } else {
+    result.innerText = "선택된 취미가 없습니다.";
+  }
 }
---
+++ 실습과제/241115_회원가입.js
@@ -0,0 +1,52 @@
+function 약관동의() {
+  /*const agree1 = document.getElementById("동의1").checked;
+  const agree2 = document.getElementById("동의2").checked;
+  const agree3 = document.getElementById("동의3").checked;
+
+  const agreeAll = agree1 && agree2 && agree3;
+
+  document.getElementById("sighup").disabled = !agreeAll;*/
+
+  const agree1 = document.getElementById("동의1").checked;
+  const agree2 = document.getElementById("동의2").checked;
+  const agree3 = document.getElementById("동의3").checked;
+  const agreeAll = agree1 && agree2 && agree3;
+  if (agreeAll) {
+    window.location.href = "241115_회원가입1.html";
+  } else {
+    alert("모든 필수 약관에 동의해야 합니다.");
+  }
+}
+
+//1. 가입버튼 가입이 완료 되었다는 alert 이후 성공.html 이동하기
+//2. addEventListener로 변경해서 작성
+function 회원가입동의() {
+  const id = document.getElementById("id").value;
+  const pw = document.getElementById("pw").value;
+  const email = document.getElementById("email").value;
+  const idRgeExp = /^[가-힇0-9a-zA-Z]{5,12}$/;
+  /*
+  /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+  특정 문자를 포함 했는가?
+  (?=.*[A-Za-z]) = 대소문자 영어가 있는지 탐색
+  (?=.*\d)       = 숫자가 포함되어 있는가
+  영어 대/소문자나 숫자가 모두 하나이상 포함이 되어있어야 하는 비밀번호
+
+  */
+  const pwRgeExp = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+  const emailRgeExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  if (!idRgeExp.test(id)) {
+    alert("아이디는 5-12자의 영문자 및 숫자여야 합니다");
+    return false;
+  }
+  if (!pwRgeExp.test(pw)) {
+    alert("비밀번호는 최소 8자이며,문자와 수자를 포함해야합니다.");
+    return false;
+  }
+  if (!emailRgeExp.test(email)) {
+    alert("올바르지 않은 이메일 형식입니다.");
+    return false;
+  }
+  alert("회원가입이 완료되었습니다.");
+}
